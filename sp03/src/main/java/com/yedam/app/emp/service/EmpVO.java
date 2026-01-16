26db18310f45066ee99560410513758570b68016
hytkim <145666445+hytkim@users.noreply.github.com>
2026-01-16 17:53:38 +0900

    김현태/3가지표현식을 활용한CRUD기능/26-01-16

--- sp03/src/main/java/com/yedam/app/emp/service/EmpVO.java
+++ sp03/src/main/java/com/yedam/app/emp/service/EmpVO.java
@@ -2,6 +2,8 @@ package com.yedam.app.emp.service;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
@@ -11,6 +13,8 @@ public class EmpVO {
 	private String lastName;      // Not Null
 	private String email;         // Not Null, Uk
 	private String phoneNumber;
+	// java.util.Date : yy/MM/dd
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date hireDate;        // Not Null 
 	private String jobId;         // Not Null, FK
 	private double salary;
